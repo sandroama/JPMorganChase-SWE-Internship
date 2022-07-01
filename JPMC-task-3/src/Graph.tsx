a423072d87c196d7bd79f9086de8702e06d32f72
SandroAma <sandroama123@gmail.com>
2022-07-01 03:53:00 -0400

    finish task 3

--- JPMC-task-3/src/Graph.tsx
+++ JPMC-task-3/src/Graph.tsx
@@ -55,9 +55,9 @@ class Graph extends Component<IProps, {}> {
 
   componentDidUpdate() {
     if (this.table) {
-      this.table.update(
-        DataManipulator.generateRow(this.props.data),
-      );
+      this.table.update([
+        DataManipulator.generateRow({ serverResponds: this.props.data }),
+      ]);
     }
   }
 }
